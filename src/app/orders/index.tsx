e98035aa58fc490ff886253b8f0ae6c237c4ca47
Abdubek <zholay.abdubek@gmail.com>
2025-01-27 15:09:24 +0500

    fix orders date

--- src/app/orders/index.tsx
+++ src/app/orders/index.tsx
@@ -10,6 +10,7 @@ import {
 import { useQuery } from "@tanstack/react-query";
 import { api } from "../../shared/api";
 import { TransactionStatus } from "../../shared/api/transactions.ts";
+import dayjs from "dayjs";
 
 export function OrdersPage() {
   const { data } = useQuery({
@@ -28,17 +29,19 @@ export function OrdersPage() {
         <IonList lines="full">
           {data?.data.transactions.map((item, index) => (
             <IonItem key={index}>
-              <div className="w-full py-2">
-                <div className="flex items-center justify-between">
+              <div className="w-full py-4">
+                <div className="flex items-center justify-between py-2">
                   <span className="text-lg">Заказ №{item.id}</span>
                   <IonBadge className="p-1 text-xs">
                     {getStatusText(item.status)}
                   </IonBadge>
                 </div>
-                <div className="text-gray-400 text-sm">
+                <div className="text-gray-400 text-sm pb-2">
                   {item.items.map((i) => i.name).join(", ")}
                 </div>
-                <div className="text-gray-400 text-sm">{item.pickup_time}</div>
+                <div className="text-gray-400 text-sm">
+                  {dayjs(item.pickup_time).format("DD-MM-YYYY HH:mm")}
+                </div>
               </div>
             </IonItem>
           ))}
